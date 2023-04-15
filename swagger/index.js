9b9c0768b96cbc6e06466b668b0da8a349dca6ad
Shuntaro Okuma <s-ohkuma@digitalblast.co.jp>
2023-04-16 03:49:25 +0900

    fix swagger settings

--- swagger/index.js
+++ swagger/index.js
@@ -6,20 +6,7 @@ const swaggerDocument = YAML.load("./openapi.yaml");
 const app = express();
 const PORT = 3001;
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get(
-  "/",
-  swaggerUi.setup(null, {
-    swaggerOptions: {
-      requestInterceptor: function (request) {
-        request.headers.Origin = `http://localhost:3001`;
-        return request;
-      },
-      url: `http://localhost:3001/docs/`,
-    },
-  })
-);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Swagger UI is available at http://localhost:${PORT}`);
